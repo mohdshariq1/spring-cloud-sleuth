434ba7c8a78d3db386d552e26cfae04dd9ef3066
mohdshariq1 <mohdshariq@gmail.com>
2017-06-11 01:55:05 +0530

    Create SampleZipkinApplication.java

--- spring-cloud-sleuth-samples/spring-cloud-sleuth-sample-zipkin/src/main/java/sample/SampleZipkinApplication.java
+++ spring-cloud-sleuth-samples/spring-cloud-sleuth-sample-zipkin/src/main/java/sample/SampleZipkinApplication.java
@@ -48,7 +48,7 @@ public class SampleZipkinApplication {
 
 	// Use this for debugging (or if there is no Zipkin server running on port 9411)
 	@Bean
-	@ConditionalOnProperty(value = "sample.zipkin.enabled", havingValue = "false")
+	@ConditionalOnProperty(value = "sample.zipkin.enabled", havingValue = "true")
 	public ZipkinSpanReporter spanCollector() {
 		return new ZipkinSpanReporter() {
 			@Override
